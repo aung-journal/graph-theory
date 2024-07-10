92dd5cfd940426dea32901f1cff702fce2746e86
Aung Pyae Phyo <lftopalfredlames@gmail.com>
2024-07-10 19:00:59 +0630

    First

--- fw/fw.py
+++ fw/fw.py
@@ -1,84 +1,122 @@
-from setup import nx, bf_edges_1
+from setup import nx
 import time
 
+edges = [
+    (0, 1, 2),
+    (1, 2, -1),
+    (2, 3, 3),
+    (3, 4, 2),
+    (4, 5, 1),
+    (0, 2, 4),
+    (2, 4, 2),
+    (1, 3, 7),
+    (3, 1, -2)  # This edge introduces a negative weight
+]
+
+edges_1 = [
+    (0, 1, 2),
+    (1, 2, -1),
+    (2, 3, 3),
+    (3, 4, 2),
+    (4, 5, 1),
+    (5, 6, 2),
+    (6, 7, -3),
+    (7, 8, 2),
+    (8, 9, 1),
+    (9, 10, 4),
+    (10, 11, -2),
+    (11, 12, 1),
+    (12, 13, 3),
+    (13, 14, 2),
+    (14, 15, 1),
+    (15, 16, -1),
+    (16, 17, 2),
+    (17, 18, 1),
+    (18, 19, -2),
+    (0, 2, 4),
+    (2, 4, 2),
+    (1, 3, 7),
+    (3, 1, -2),
+    (10, 0, -4),  # Ensures some connectivity in the graph
+    (8, 15, 5)
+]
+
 g = nx.DiGraph()
-for u, v, w in bf_edges_1:
+for u, v, w in edges_1:
     g.add_edge(u, v, weight=w)
 
 n = g.number_of_nodes()
-dp = [[] for _ in range(n)]
-next = [[None] for _ in range(n)]
+dp = [[float('inf')] * n for _ in range(n)]
+next_matrix = [[None] * n for _ in range(n)]
 
-def setup(m: nx.Graph):
-    #do a deep copy of the input matrix and setup
-    #the 'next' matrix for path reconstruction
+def setup(graph: nx.Graph):
     for i in range(n):
         for j in range(n):
-            dp[i][j] = m[i][j]
-            if m[i][j] != float('inf'):
-                next[i][j] = j
+            if i == j:
+                dp[i][j] = 0
+            elif graph.has_edge(i, j):
+                dp[i][j] = graph[i][j]['weight']
+                next_matrix[i][j] = j
+            else:
+                dp[i][j] = float('inf')
+                next_matrix[i][j] = None
 
-def propagateNegativeCycles(dp: list[list[int]], n: int):
-    #Execute FW ASAP algorithm a second time but
-    #this time if the dist can be improved
-    #set the optimal dist to be -infinity.
-    #every edge (i, j) is marked with -infinit is either
-    #part of or reaches into a neg cycle.
+def propagateNegativeCycles():
     for k in range(n):
         for i in range(n):
             for j in range(n):
                 if dp[i][k] + dp[k][j] < dp[i][j]:
                     dp[i][j] = float('-inf')
-                    next[i][j] = -1
+                    next_matrix[i][j] = -1
 
-def floydWarshall(m: nx.Graph):
-    setup(m)
+def floydWarshall(graph: nx.Graph):
+    setup(graph)
 
-    #Execute FW all pairs shortest path algorithm
     for k in range(n):
         for i in range(n):
             for j in range(n):
                 if dp[i][k] + dp[k][j] < dp[i][j]:
                     dp[i][j] = dp[i][k] + dp[k][j]
-                    next[i][j] = next[i][k]
+                    next_matrix[i][j] = next_matrix[i][k]
 
-    #Detect and propagate negative cycles
-    #This is an optional 
-    propagateNegativeCycles(dp, n)
+    propagateNegativeCycles()
+    return dp
 
 def reconstructPath(s, e):
     path = []
-    #Check if there exists a path between the start
-    #and end node
-    if dp[s][e] == float('inf'): return path
+    if dp[s][e] == float('inf'):
+        return path
 
     at = s
-    # Reconstruct path from the next matrix
     while at != e:
-        at = next[at][e]
-        #the start node is trapped in negative cycle
-        if at == -1: return None
+        if at == -1:
+            return None
         path.append(at)
-    
-    if next[at][e] == -1: return None
+        at = next_matrix[at][e]
+
+    if next_matrix[at][e] == -1:
+        return None
     path.append(e)
     return path
 
 def main():
-
     start_time = time.time()
     floydWarshall(g)
     print("The time taken to create ASAP is --- {} seconds ---\n".format(time.time() - start_time))
 
     quit = False
     while not quit:
-        start = int(input("Enter the start number you want to use dijkstra for (1 - {}) : ".format(g.number_of_nodes())))
-        end = int(input("Enter the end number you want to use dijkstra for (1 - {}) : ".format(g.number_of_nodes())))
+        start = int(input("Enter the start number you want to use Floyd-Warshall for (0 - {}) : ".format(n-1)))
+        end = int(input("Enter the end number you want to use Floyd-Warshall for (0 - {}) : ".format(n-1)))
 
         start_time = time.time()
-        print(f'The shortest path from {start} to {end} is ' + '[' + ', '.join([str(i) for i in reconstructPath(g, start, end)]) + ']')
+        path = reconstructPath(start, end)
+        if path:
+            print(f'The shortest path from {start} to {end} is ' + '[' + ', '.join([str(i) for i in path]) + ']')
+        else:
+            print(f'No path exists from {start} to {end}')
         print("The time taken is --- {} seconds ---".format(time.time() - start_time))
-        quit = input('Do you want to quit(Y/N): ').lower() == 'y'
+        quit = input('Do you want to quit (Y/N): ').lower() == 'y'
 
 if __name__ == '__main__':
     main()
